14db2ae1dfbcd164f16fcaa8bc09da89b49dac33
eagleweb <eagleweb@mail.ru>
2017-12-13 17:04:22 +0200

    Start

---
+++ public/assets/js/slick-slider.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('.main-slider').slick({
+        nextArrow: false,
+        prevArrow: false,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 8000
+    });
+});
+
